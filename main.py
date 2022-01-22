f595352ed633be4a5e0b3d7e5a6d6850f498a56b
kei-kusanagi <kei_kusanagi@outlook.com>
2022-01-21 22:24:02 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+path = "Copia de ETIQUETAR inventario 2015.xlsx"
+df = pd.read_excel(path)
+
+
+def busqueda(df):
+    solicitud = input("Codigo a buscar: ")
+    BUSQUEDA = [solicitud.upper()]
+
+    df["check"] = df["CODIGO"].isin(BUSQUEDA)
+
+    DESC = [df.loc[df.check == True,'DESCRIPCION']]
+    SER = [df.loc[df.check == True,'SERIE']]
+
+    return DESC, SER
+
+
+print(busqueda(df))
